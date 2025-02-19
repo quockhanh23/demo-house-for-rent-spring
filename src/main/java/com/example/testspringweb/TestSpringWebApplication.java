faffc463a8bf19f2f4dc4d743f9d3f301bfbbcd7
Admin <yeuthuongvoivangdiqua@gmail.com>
2025-02-19 20:32:59 +0700

    first commit

---
+++ src/main/java/com/example/testspringweb/TestSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.testspringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringWebApplication.class, args);
+    }
+
+}
